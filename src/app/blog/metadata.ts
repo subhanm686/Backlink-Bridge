847ce5b406f1590171c6060245076c023f85134f
Subhan Masood <subhanmasood1999@gmail.com>
2026-06-11 19:44:54 +1000

    Create metadata.ts

---
+++ src/app/blog/metadata.ts
@@ -0,0 +1,7 @@
+import { constructMetadata } from '@/lib/metadata'
+
+export const metadata = constructMetadata({
+  title: 'Link Building & SEO Strategy Blog | Backlink Bridge',
+  description: 'Expert guides on link building, guest posting, DR/DA improvement, spam score reduction, and off-page SEO strategy. Written by Muhammad Subhan, founder of Backlink Bridge.',
+  canonicalUrl: 'https://backlinkbridge.com/blog',
+})
