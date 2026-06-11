9d19a1576824574d2a8fdf249d7f47cffc94d827
Subhan Masood <subhanmasood1999@gmail.com>
2026-06-11 11:42:11 +1000

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import Script from 'next/script'
 import Navbar from '@/components/layout/Navbar'
 import Footer from '@/components/layout/Footer'
 import './globals.css'
+import { OrganizationSchema, ServiceSchema } from "@/components/SchemaMarkup";
 
 const inter = Inter({
   subsets: ['latin'],
