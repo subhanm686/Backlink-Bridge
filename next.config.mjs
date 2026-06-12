07a2d2e4ef72bd6b1cf2de5a51904202fd9f5c2c
Subhan Masood <subhanmasood1999@gmail.com>
2026-06-12 20:40:04 +1000

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,20 @@ const nextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/blog/improve-domain-rating-2025',
+        destination: '/blog/improve-domain-rating-2026',
+        permanent: true,
+      },
+      {
+        source: '/blog/reduce-spam-score-guide',
+        destination: '/blog/reduce-spam-score-guide-2026',
+        permanent: true,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
