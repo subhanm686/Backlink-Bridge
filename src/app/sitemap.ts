b4033cbff54b773469203dff95bfcc342bd6e834
Subhan Masood <subhanmasood1999@gmail.com>
2026-06-13 00:08:45 +1000

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -112,6 +112,24 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'monthly',
       priority: 0.8,
     },
+    {
+  url: `${baseUrl}/privacy`,
+  lastModified: new Date(),
+  changeFrequency: 'monthly',
+  priority: 0.4,
+},
+{
+  url: `${baseUrl}/terms`,
+  lastModified: new Date(),
+  changeFrequency: 'monthly',
+  priority: 0.4,
+},
+{
+  url: `${baseUrl}/refund`,
+  lastModified: new Date(),
+  changeFrequency: 'monthly',
+  priority: 0.4,
+},
    {
       url: `${baseUrl}/contact`,
       lastModified: new Date(),
