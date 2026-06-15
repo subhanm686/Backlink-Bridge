e8f446583d076ce5921a62f23ca9fca55093eccb
Subhan Masood <subhanmasood1999@gmail.com>
2026-06-15 12:01:21 +1000

    Update robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: '/dashboard',
+      disallow: ['/dashboard', '/niches?'],
     },
     sitemap: 'https://backlinkbridge.com/sitemap.xml',
   }
